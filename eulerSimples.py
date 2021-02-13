c8a7a13f36ce4680811fdc8ffdbc24bf79a66434
Jefferson Costa <jefficostau@gmail.com>
2021-02-13 17:54:01 -0300

    update

--- eulerSimples.py
+++ eulerSimples.py
@@ -25,13 +25,6 @@ def function(x, y):
 
 def eulerSimples(x0, y0, h, x): 
     yn = y0
-    
-    #aqui interamos até o ponto que queremos a apróximação
-    #y1 = y0 + h * func(x0, y0) 
-    #y2 = y1 + h * func(x0, y1)
-    #.
-    #.
-    #.
 
     while x0 < x: 
         yn1 = yn + h * function(x0, yn) 
